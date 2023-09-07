7ad38484b27206c8bde5379594716d74ffc8d410
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-09-07 19:56:25 +0300

    FRONTEND: fix basename

--- frontend/src/pages/RouterPage.tsx
+++ frontend/src/pages/RouterPage.tsx
@@ -4,9 +4,11 @@ import { BrowserRouter, Route,Routes } from 'react-router-dom';
 import { SkillsPage } from './SkillsPage';
 import { VacancyLinkPage } from './VacancyLinkPage';
 
+const routerBaseName = process.env.NODE_ENV === 'development' ? '/' : 'itmo-bootcamp23';
+
 const RouterPage = () => {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename={routerBaseName}>
       <Routes>
         <Route path="/" Component={VacancyLinkPage} />
         <Route path="/skills" Component={SkillsPage} />
